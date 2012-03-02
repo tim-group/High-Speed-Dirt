9195d45c6067a16beccfc0f4e3a028f3421c653e
Dominic Fox <dominic.fox@gmail.com>
2012-03-02 14:17:14 +0000

    Traverse 100,000 records in 0.15s

--- src/test/java/com/youdevise/hsd/HSQLDBIntegrationTest.java
+++ src/test/java/com/youdevise/hsd/HSQLDBIntegrationTest.java
@@ -6,7 +6,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -35,7 +38,7 @@ public class HSQLDBIntegrationTest {
                 "       name VARCHAR(255),",
                 "       primary key(id));");
 
-        for (int i=0; i<1000; i++) {
+        for (int i=0; i<100000; i++) {
             executeStatement("INSERT INTO test (name) values ('",
                              Integer.toString(i), "');");
         }
@@ -56,24 +59,47 @@ public class HSQLDBIntegrationTest {
     }
     
     public class Handler {
+        public int count = 0;
         public boolean handle(int id, String name) {
-            System.out.println(name);
+            count += 1;
             return true;
         }
     }
     
     @Test public void
     passes_retrieved_record_fields_to_handler() throws Exception {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT id, name FROM test");
-        EnumIndexedCursor<Fields> cursor = ResultSetAdapter.adapting(resultSet, Fields.class);
         Handler handler = new Handler();
+        Date before = new Date();
+        
+        executeQuery(handler);
+        
+        Date after = new Date();
+        System.out.println(String.format("Traversed 100000 records in %s milliseconds", after.getTime() - before.getTime()));
+        
+        MatcherAssert.assertThat(handler.count, Matchers.equalTo(100000));
+    }
+
+    private void executeQuery(Handler handler) throws SQLException, NoSuchMethodException {
+        Statement statement = connection.createStatement();
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT id, name FROM test");
+            try {
+                traverseResults(resultSet, handler);
+            } finally {
+                resultSet.close();
+            }
+        } finally {
+            statement.close();
+        }
+    }
+
+    private void traverseResults(ResultSet resultSet, Handler handler) throws NoSuchMethodException {
+        EnumIndexedCursor<Fields> cursor = ResultSetAdapter.adapting(resultSet, Fields.class);
         Method method = Handler.class.getMethod("handle", Integer.TYPE, String.class);
         MethodDispatcher<Handler, Fields> factory = MethodDispatcherFactory.dispatching(Handler.class, method, Fields.class, Fields.id, Fields.name);
         EnumIndexedCursorHandler<Fields> cursorHandler = factory.to(handler);
         while (cursor.next()) {
             cursorHandler.handle(cursor);
         }
-        
     }
 }
